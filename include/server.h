7c7f97eeb88198c020e205d07760fdd4d2bda78f
Sinh Ngo <omaken567@gmail.com>
2025-09-07 21:41:33 +0700

    server

--- include/server.h
+++ include/server.h
@@ -1,1 +1,11 @@
+#ifndef SERVER_H
+#define SERVER_H
 
+#include <winsock2.h>
+#include "config.h"
+
+int server_init(const char *listen_host, int port, SOCKET *server_fd);
+void start_server(const Proxy_Config *config);
+void server_cleanup(SOCKET server_fd);
+
+#endif
