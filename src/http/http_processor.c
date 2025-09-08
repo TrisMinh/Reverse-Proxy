538a53be756a38b965d1246e551082af42c64331
TrungSy2106 <dinhcongtrungduc@gmail.com>
2025-09-08 10:04:38 +0700

    update

--- src/http/http_processor.c
+++ src/http/http_processor.c
@@ -5,7 +5,6 @@
 int validate_http_request(const char *request) {
     if (!request || strlen(request) < 10) return 0;
     if (!strstr(request, " HTTP/1.")) return 0;
-    // Kiểm tra request có hợp lệ không (basic validation)
     if (strncmp(request, "GET ", 4) == 0 ||
         strncmp(request, "POST ", 5) == 0 ||
         strncmp(request, "PUT ", 4) == 0 ||
@@ -79,4 +78,62 @@ int modify_request_headers(const char *original_req, char *modified_req, int max
     
     // Không có Host header
     return -1;
+}
+
+// Xử lý response từ backend trước khi gửi về client
+void modify_response_headers(const char *original_resp, char *modified_resp, int max_len, const char *backend_host, int backend_port, const char *proxy_host, int proxy_port) {
+    
+    strcpy(modified_resp, original_resp);
+    
+    char *location = strstr(modified_resp, "Location: http://");
+    if (!location) {
+        location = strstr(modified_resp, "location: http://");
+    }
+    
+    if (!location) {
+        return;
+    }
+    
+    char *location_end = strstr(location, "\r\n");
+    if (!location_end) {
+        return;
+    }
+    
+    char *location_value = location + 9;
+    while (*location_value == ' ') location_value++;
+    
+    char backend_url_with_port[128];
+    char backend_url_no_port[128];
+    snprintf(backend_url_with_port, sizeof(backend_url_with_port), "http://%s:%d", backend_host, backend_port);
+    snprintf(backend_url_no_port, sizeof(backend_url_no_port), "http://%s", backend_host);
+    
+    int need_fix = 0;
+    char *path_to_keep = NULL;
+    
+    if (strncmp(location_value, backend_url_with_port, strlen(backend_url_with_port)) == 0) {
+        need_fix = 1;
+        path_to_keep = location_value + strlen(backend_url_with_port);
+    }
+    else if (strncmp(location_value, backend_url_no_port, strlen(backend_url_no_port)) == 0) {
+        need_fix = 1;
+        path_to_keep = location_value + strlen(backend_url_no_port);
+    }
+    
+    if (need_fix) {
+        char proxy_url[128];
+        snprintf(proxy_url, sizeof(proxy_url), "http://%s:%d", proxy_host, proxy_port);
+        
+        char new_response[8192];
+        int before_len = location_value - modified_resp;
+        int after_start = location_end - modified_resp;
+        
+        snprintf(new_response, sizeof(new_response), "%.*s%s%s%s",
+                before_len, modified_resp,
+                proxy_url, path_to_keep,
+                modified_resp + after_start);
+        
+        if (strlen(new_response) < max_len) {
+            strcpy(modified_resp, new_response);
+        }
+    }
 }
