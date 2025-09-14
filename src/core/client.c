70ec4b0189eaf16ea8d8507972a4b7fb192967e5
TrisMinh <minhtridn05@gmail.com>
2025-09-14 21:35:18 +0700

    nothing

--- src/core/client.c
+++ src/core/client.c
@@ -1,12 +1,15 @@
 #include "../include/client.h"
 #include "../include/http_processor.h"
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0600
+#undef _WIN32_WINNT
+#define _WIN32_WINNT 0x0601
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 #endif
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #ifdef _MSC_VER
 #pragma comment(lib, "ws2_32.lib")
 #endif
@@ -14,27 +17,35 @@
 #define BUFFER_SIZE 8192
 
 int connect_to_backend(const char *host, int port, SOCKET *out_sock) {
-    struct sockaddr_in backend_addr;
+    struct addrinfo hints, *res, *p;
+    char port_str[16];
+    int rv;
 
-    *out_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (*out_sock == INVALID_SOCKET) {
+    snprintf(port_str, sizeof(port_str), "%d", port);
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;       // chỉ IPv4
+    hints.ai_socktype = SOCK_STREAM; // TCP
+
+    if ((rv = getaddrinfo(host, port_str, &hints, &res)) != 0) {
         return -1;
     }
 
-    backend_addr.sin_family = AF_INET;
-    backend_addr.sin_port = htons(port);
-    unsigned long addr = inet_addr(host);
-    if (addr == INADDR_NONE) {
+    for (p = res; p != NULL; p = p->ai_next) {
+        *out_sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (*out_sock == INVALID_SOCKET) continue;
+
+        
+        if (connect(*out_sock, p->ai_addr, (int)p->ai_addrlen) == 0) {
+            freeaddrinfo(res);
+            return 0; // OK
+        }
+
         closesocket(*out_sock);
-        return -1;
-    }
-    backend_addr.sin_addr.s_addr = addr;
-
-    if (connect(*out_sock, (struct sockaddr*)&backend_addr, sizeof(backend_addr)) == SOCKET_ERROR) {
-        closesocket(*out_sock);
-        return -1;
     }
 
-    return 0;
+    freeaddrinfo(res);
+    return -1;
 }
+
 
